bd9b760ebb25366e82339857254ed132a4a1aceb
filmm0re <103958897+filmm0re@users.noreply.github.com>
2022-12-02 09:08:55 +0100

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,5 +1,5 @@
 def check_continue() -> bool:
-    return validate_continue(input('Do you want to add more numbers? [y/n] '))
+    return validate_continue(input('Do you want to add more numbers? [d/n] '))
 
 
 def validate_continue(s: str) -> bool:
